7b9debbb0e9f46c5e113b3dd693454d57a9e2564
Naba Yousuf <nabay0519@gmail.com>
2025-08-17 05:51:34 +0500

    setup EJS view engine, static files, and middleware

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+//to use public and views folder
+const path = require("path");
+
+// Parse incoming request data
+app.use(express.urlencoded({extended:true}));
+
+//set view engine
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.set(express.static(path.join(__dirname,"public")));
+
+// Basic route
+app.get("/",(req,res)=>{
+    res.send("server working well");
+})
 
 app.listen(port , ()=> {
     console.log("listening on port 8080");
